ab3b06f210eb98bdfac0392a8dd67b3db81cf986
Kumarn00 <kumarnitish842248@gmail.com>
2024-10-22 14:43:56 +0530

    first commit

--- src/routers/client-router.jsx
+++ src/routers/client-router.jsx
@@ -14,7 +14,7 @@ import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
 import AddBook from "../pages/dashboard/addbook/AddBook";
 import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
 import DashboardLayout from "../pages/dashboard/DashBoardLayout";
-import PrivateRoute from "./Privateroute.jsx";
+import PrivateRoute from "./Privateroute";
 
 
 
